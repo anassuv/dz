5eb21c8969fea6f574f17dbca52a9788621275d4
anassuv <114801727+anassuv@users.noreply.github.com>
2022-11-05 09:26:55 +0300

    задача 6

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
-﻿ Console.Write("a = ");
-            double a = double.Parse(Console.ReadLine());
-            Console.Write("b = ");
-            double b = double.Parse(Console.ReadLine());
-            Console.Write("c = ");
-            double c = double.Parse(Console.ReadLine());
-            if (a > b) a = b;
-            if (a > c) a = c;
-            Console.WriteLine("max = {0}",a);
-
+﻿int a;
+a = int.Parse(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("четное");
+}
+else
+{
+   Console.WriteLine("нечетное"); 
+}
